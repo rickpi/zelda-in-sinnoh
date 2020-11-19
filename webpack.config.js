077d5686ee9707bd881405f0c834c03df91948b4
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-11-19 22:12:18 +0100

    chore(babel): upgrade env to babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-            {loader :'babel-loader'},
+            {
+              loader :'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            },
             {loader :'eslint-loader',
               options: {
                 failOnError: true,
